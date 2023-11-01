1a8349679cadf6e234baf68adfde96f021e82cfb
Artem Zhuk <artem.zx656@gmail.com>
2023-11-02 01:32:00 +0300

    added 1st seminar code

--- app/views.py
+++ app/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+QUESTIONS = [
+    {
+        'id': i,
+        'title': f'Question {i}',
+        'likes': i,
+        'content': f'Lorem Ipsum {i}',
+    } for i in range(20)
+]
+
+
+def paginate(objects, page, per_page=15):
+    paginator = Paginator(QUESTIONS, per_page)
+    return paginator.page(page)
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    page = request.GET.get('page', 1)
+    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page)})
 
-def question(request):
-    return render(request, 'question.html')
+
+def question(request, question_id):
+    item = QUESTIONS[question_id]
+    return render(request, 'question.html', {'question': item})

af533adb0f774a6602884101be93aacfe849f615
Artem Zhuk <artem.zx656@gmail.com>
2023-11-02 18:12:11 +0300

    hw2 without correct pagination

--- Web_QA/urls.py
+++ Web_QA/urls.py
@@ -20,7 +20,11 @@ from app import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('hot', views.hot, name='hot'),
     path('ask', views.ask, name='ask'),
     path('question/<int:question_id>', views.question, name='question'),
+    path('tag/<tag_id>', views.tag, name='tag'),
+    path('login', views.login, name='login'),
+    path('signup', views.signup, name='signup'),
     path('admin/', admin.site.urls),
 ]
--- app/views.py
+++ app/views.py
@@ -7,11 +7,27 @@ QUESTIONS = [
         'title': f'Question {i}',
         'likes': i,
         'content': f'Lorem Ipsum {i}',
+        'tags': ['CSS', 'Bauman', 'cats'],
     } for i in range(20)
 ]
 
+QUESTION_REPLY = [
+    {
+        'content': 'Look at dplyr as a back-end agnostic interface, with all of the targets'
+                   'using the same grammer, where you can extend the targets and handlers at will.'
+                   'data.table is, from the dplyr perspective, one of those targets.',
+        'likes': (3 - i),
+    } for i in range(3)
+]
 
-def paginate(objects, page, per_page=15):
+STATS = {
+    'tags': ['CSS', 'Bamonka', 'offtopic', 'cats', 'memes', 'C++ Programming'],
+    'best_members': ['Mr. Freeman', 'Max Payne', 'Carl Johnson', 'Geralt of Rivia', 'John Tanner']
+}
+
+
+def paginate(request, objects, per_page=15):
+    page = request.GET.get('page', 1)
     paginator = Paginator(QUESTIONS, per_page)
     return paginator.page(page)
 
@@ -19,18 +35,34 @@ def paginate(objects, page, per_page=15):
 # Create your views here.
 def index(request):
     page = request.GET.get('page', 1)
-    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page), 'page_title'  : 'Questions'})
+    return render(request, 'index.html',
+                  {'questions': paginate(request, QUESTIONS), 'page_title': 'Questions', 'stats': STATS})
 
 
 def question(request, question_id):
     item = QUESTIONS[question_id]
-    return render(request, 'question.html', {'question': item})
+    replies = QUESTION_REPLY
+    return render(request, 'question.html', {'question': item, 'replies': replies, 'stats': STATS})
 
 
 def ask(request):
-    return render(request, 'ask.html')
+    return render(request, 'ask.html', {'stats': STATS})
 
 
 def hot(request):
     page = request.GET.get('page', 1)
-    return render(request, 'index.html', {'questions': paginate(QUESTIONS, page)})
+    return render(request, 'index.html',
+                  {'questions': paginate(request, QUESTIONS), 'page_title': 'Hot Questions', 'stats': STATS})
+
+
+def tag(request, tag_id):
+    return render(request, 'index.html',
+                  {'questions': paginate(request, QUESTIONS), 'page_title': f'{tag_id}:', 'stats': STATS})
+
+
+def login(request):
+    return render(request, 'login.html', {'stats': STATS})
+
+
+def signup(request):
+    return render(request, 'signup.html', {'stats': STATS})
